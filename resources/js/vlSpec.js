fe9de7e5ca70cd71df97dd198df03167a6f3b20d
Jairo Ruiz Saenz <jairoruizsaenz@gmail.com>
2018-09-07 12:23:32 -0500

    Update

--- resources/js/vlSpec.js
+++ resources/js/vlSpec.js
@@ -1,5 +1,6 @@
 // Assign the specification to a local variable vlSpec.
-const vlSpec = "resources/json/ResultadosEncuesta.json";
+//const vlSpec = "resources/json/ResultadosEncuesta.json";
+const vlSpec = "resources/json/TEST.json";
 
 // Embed the visualization in the container with id `vis`
 vegaEmbed("#vis", vlSpec);
